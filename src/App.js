c8fbebdf9e334915012022265f73a2432975c26c
achadha2 <andy.chadha2@gmail.com>
2019-06-25 14:31:04 -0400

    set up skeleton for styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled, { keyframes } from "styled-components";
+
+const AppWrapper = styled.div`
+  text-align: center;
+`;
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 120s linear;
+  height: 250px;
+  &:hover {
+    animation: ${rotate360} infinite 1.5s linear;
+  }
+`;
+const AppHeader = styled.div`
+  background-color: #222;
+  height: 300px;
+  padding: 20px;
+  color: white;
+`;
+const AppTitle = styled.h1`
+  font-size: 1.3em;
+`;
+const AppIntro = styled.p`
+  font-size: large;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
+      </AppHeader>
+      <AppTitle>Welcome to react</AppTitle>
+      <AppIntro>Testing styled components!</AppIntro>
+    </AppWrapper>
   );
 }
 
